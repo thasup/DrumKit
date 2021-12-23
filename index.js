b7062ec6f3c02eb6b137fc3c7075308639243caa
thasup <thanachonfirst@hotmail.com>
2021-12-23 07:11:30 +0700

    listen to keydown

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const drums = $(".drum");
+function playSound(name) {
+    let audio = new Audio(`./sounds/${name}.mp3`);
+    audio.play();
+};
 
-$(document).ready(() => {
-    for (let i = 0; i < drums.length+1 ; i++) {
-        $(`.set .drum:nth-child(${i}`).click(() => {
-            console.log(`bong! ${$(`.set .drum:nth-child(${i}`).text()}`);
-           
-        });
+for (let i = 0; i < $(".drum").length + 1; i++) {
+    $(`.set .drum:nth-child(${i}`).on("click", function () {
+        $(this).css("color", "white");
+
+        console.log($(this).text());
+        makeSound($(this).text());
+
+    });
+}
+
+$(document).keydown(function (event) {
+    console.log(event.key);
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            playSound("tom-1");
+            break;
+        case "s":
+            playSound("tom-2");
+            break;
+        case "d":
+            playSound("tom-3");
+            break;
+        case "f":
+            playSound("tom-4");
+            break;
+        case "j":
+            playSound("crash");
+            break;
+        case "k":
+            playSound("kick-bass");
+            break;
+        case "l":
+            playSound("snare");
+            break;
+        default: console.log(key);
+            break;
     }
-})
+}
